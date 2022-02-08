1eff000c1c2cfea4bd41e5732a59e5910a9ccba3
Ajay K <ajaycoolkarni@yahoo.com>
2022-02-08 23:17:03 +1030

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  process.env.MONGODB_URI || 'mongodb://localhost/google-book-search',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
